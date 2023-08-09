4fd52aeff9aa92f4e7d4d3b9b6aa0d34f7bcef3b
신창학 <csg1353@naver.com>
2023-08-09 18:01:00 +0900

    feat : schdular TEST 예제 적용 완료

--- backend/damda/src/main/java/com/b210/damda/util/schedular/ScheduledComponent.java
+++ backend/damda/src/main/java/com/b210/damda/util/schedular/ScheduledComponent.java
@@ -12,12 +12,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class ScheduledComponent {
+//    @Scheduled(fixedRate = 2000) // 1초마다 실행
+//    public void scheduledTask() {
+//        log.info("스케줄링된 작업 실행: {}", System.currentTimeMillis());
+//        // userFluxSinkMap에 접근
+//        log.info("size : {}", EventStreamService.userFluxSinkMap.size());
+//    }
 
-
-    @Scheduled(fixedRate = 2000) // 1초마다 실행
-    public void scheduledTask() {
-        log.info("스케줄링된 작업 실행: {}", System.currentTimeMillis());
-        // userFluxSinkMap에 접근
-        log.info("size : {}", EventStreamService.userFluxSinkMap.size());
+    @Scheduled(cron = "0 39 17 * * *", zone = "Asia/Seoul") // 매일 00시 00분 00초
+    public void scheduledEvent() {
+        // 여기에 특정 이벤트를 발생시키는 로직을 작성
+        System.out.println("특정 이벤트 발생: " + System.currentTimeMillis());
     }
 }
